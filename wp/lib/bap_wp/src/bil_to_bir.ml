048c89b65f737684370c9ded025a4117c9a38079
Cody Roux <croux@draper.com>
2019-05-23 16:41:55 -0400

    Big commit releasing all of our BAP WP functionality

---
+++ wp/lib/bap_wp/src/bil_to_bir.ml
@@ -0,0 +1,122 @@
+(***************************************************************************)
+(*                                                                         *)
+(*  Copyright (C) 2018 The Charles Stark Draper Laboratory, Inc.           *)
+(*                                                                         *)
+(*  This file is provided under the license found in the LICENSE file in   *)
+(*  the top-level directory of this project.                               *)
+(*                                                                         *)
+(*  This work is funded in part by ONR/NAWC Contract N6833518C0107.  Its   *)
+(*  content does not necessarily reflect the position or policy of the US  *)
+(*  Government and no official endorsement should be inferred.             *)
+(*                                                                         *)
+(***************************************************************************)
+
+(**
+   This module allows creating simple subroutines based on the
+   BIL quasi-quoting notations.
+
+   [example]
+
+   Note that not all BIL instructions are supported, only
+   [x := e], [while_ e b] and [if_ e a b] are for now. *)
+
+open !Core_kernel
+open Bap.Std
+
+
+let mk_assert_fail () : Sub.t * Exp.t =
+  let call_tid = Tid.create () in
+  Tid.set_name call_tid "__assert_fail";
+  let call_sub = Sub.create ~tid:call_tid ~name:"__assert_fail" () in
+  call_sub, Bil.unknown (Tid.to_string call_tid) reg64_t
+
+
+let rec stmt_to_blks (stmt : Bil.Types.stmt)
+    (sub : Sub.Builder.t)
+    (tail_blk : Blk.Builder.t)
+  : Sub.Builder.t * Blk.Builder.t =
+  match stmt with
+  | Bil.Types.Move (x, e) ->
+    Blk.Builder.add_def tail_blk (Def.create x e);
+    sub, tail_blk
+  | Bil.Types.If (e, ls, rs) ->
+    let tid_l = Tid.create () in
+    let tid_r = Tid.create () in
+    let sub_l, blk_l = bil_to_blks ls sub (Blk.Builder.create ~tid:tid_l ()) in
+    (* We pass along the sub builder, to add all required blocks in both branches *)
+    let sub_r, blk_r = bil_to_blks rs sub_l (Blk.Builder.create ~tid:tid_r ()) in
+    Blk.Builder.add_jmp tail_blk (Jmp.create ~cond:e (Goto (Label.direct tid_l)));
+    Blk.Builder.add_jmp tail_blk (Jmp.create (Goto (Label.direct tid_r)));
+    let old_blk = Blk.Builder.result tail_blk in
+    Sub.Builder.add_blk sub_r old_blk;
+    let merge_tid = Tid.create () in
+    let merge_blk = Blk.Builder.create ~tid:merge_tid () in
+    let lab = Label.direct merge_tid in
+    Blk.Builder.add_jmp blk_l (Jmp.create (Goto lab));
+    let lhs_blk = Blk.Builder.result blk_l in
+    Sub.Builder.add_blk sub_r lhs_blk;
+    Blk.Builder.add_jmp blk_r (Jmp.create (Goto lab));
+    let rhs_blk = Blk.Builder.result blk_r in
+    Sub.Builder.add_blk sub_r rhs_blk;
+    sub_r, merge_blk
+
+  | Bil.Types.While (e, ss) ->
+    let exit_tid = Tid.create () in
+    let entry_tid = Tid.create () in
+    let body_tid = Tid.create () in
+    let exit_block = Blk.Builder.create ~tid:exit_tid () in
+    let entry_block = Blk.Builder.create ~tid:entry_tid () in
+    let body_block = Blk.Builder.create ~tid:body_tid () in
+    Blk.Builder.add_jmp tail_blk (Jmp.create (Goto (Label.direct entry_tid)));
+    Sub.Builder.add_blk sub (Blk.Builder.result tail_blk);
+    Blk.Builder.add_jmp entry_block
+      (Jmp.create ~cond:(Bil.lnot e) (Goto (Label.direct exit_tid)));
+    Blk.Builder.add_jmp entry_block
+      (Jmp.create (Goto (Label.direct body_tid)));
+    Sub.Builder.add_blk sub (Blk.Builder.result entry_block);
+    let sub_loop, blk_loop = bil_to_blks ss sub body_block in
+    Blk.Builder.add_jmp blk_loop
+      (Jmp.create (Goto (Label.direct entry_tid)));
+    Sub.Builder.add_blk sub_loop (Blk.Builder.result blk_loop);
+    sub_loop, exit_block
+
+  | Bil.Types.Jmp e ->
+    begin
+      match e with
+      (* This is a huge hack to enable calling "special" functions with chosen names
+         and [tid]s
+      *)
+      | Bil.Unknown (tid_s, _) ->
+        let call_tid = Tid.from_string_exn ("%" ^ tid_s) in
+        Tid.set_name call_tid "__assert_fail";
+        Blk.Builder.add_jmp tail_blk
+          (Jmp.create (Call (Call.create ~target:(Label.direct call_tid) ())));
+        sub, tail_blk
+      | _ ->
+        Blk.Builder.add_jmp tail_blk
+          (Jmp.create (Goto (Label.indirect e)));
+        sub, tail_blk
+    end
+
+  (* We currently don't handle any of these cases *)
+
+  | Bil.Types.Special _ -> assert false
+
+  | Bil.Types.CpuExn _ -> assert false
+
+and bil_to_blks (stmts : Bil.t)
+    (sub : Sub.Builder.t)
+    (tail_blk : Blk.Builder.t)
+  : Sub.Builder.t * Blk.Builder.t =
+  match stmts with
+  | [] -> sub, tail_blk
+  | s::ss ->
+    let sub', tail_blk' = stmt_to_blks s sub tail_blk in
+    bil_to_blks ss sub' tail_blk'
+
+let bil_to_sub (stmts : Bil.t) : sub term =
+  let new_sub = Sub.Builder.create () in
+  let new_blk = Blk.Builder.create () in
+  let sub, blk = bil_to_blks stmts new_sub new_blk in
+  Sub.Builder.add_blk sub (Blk.Builder.result blk);
+  Sub.Builder.result sub
---
+++ wp/lib/bap_wp/tests/performance/test_precondition.ml
@@ -0,0 +1,134 @@
+(***************************************************************************)
+(*                                                                         *)
+(*  Copyright (C) 2018 The Charles Stark Draper Laboratory, Inc.           *)
+(*                                                                         *)
+(*  This file is provided under the license found in the LICENSE file in   *)
+(*  the top-level directory of this project.                               *)
+(*                                                                         *)
+(*  This work is funded in part by ONR/NAWC Contract N6833518C0107.  Its   *)
+(*  content does not necessarily reflect the position or policy of the US  *)
+(*  Government and no official endorsement should be inferred.             *)
+(*                                                                         *)
+(***************************************************************************)
+
+open !Core_kernel
+open Bap.Std
+open OUnit2
+open Bap_wp
+open Bil_to_bir
+
+module Pre = Precondition
+module Env = Environment
+module Bool = Z3.Boolean
+module Expr = Z3.Expr
+module BV = Z3.BitVector
+
+(* To run these tests: `make test.performance` in bap_wp directory *)
+
+let mk_z3_expr e env = let e_val, _, _ = Pre.exp_to_z3 e env in e_val
+
+let format_log_error (body : string) (pre : Env.z3_expr) (post : Env.z3_expr) : string =
+  Printf.sprintf "Post:\n%s\n\nAnalyzing:\n%sPre:\n%s\n"
+    (Expr.to_string post) body (Expr.to_string (Expr.simplify pre None))
+
+let print_z3_model (ff : Format.formatter) (solver : Z3.Solver.solver)
+    (exp : 'a) (real : 'a) : unit =
+  if real = exp || real = Z3.Solver.UNSATISFIABLE then () else
+    match Z3.Solver.get_model solver with
+    | None -> ()
+    | Some model -> Format.fprintf ff "\n\nCountermodel:\n%s\n%!" (Z3.Model.to_string model)
+
+let time_z3_result (test_ctx : test_ctxt) (z3_ctx : Z3.context) (formatter : string)
+    (pre : Env.z3_expr) (expected : Z3.Solver.status) : float =
+  let solver = Z3.Solver.mk_simple_solver z3_ctx in
+  let is_correct = Bool.mk_implies z3_ctx pre (Bool.mk_false z3_ctx) in
+  let start_time = Sys.time () in
+  let result = Z3.Solver.check solver [is_correct] in
+  let end_time = Sys.time () in
+  assert_equal ~ctxt:test_ctx
+    ~printer:Z3.Solver.string_of_status
+    ~pp_diff:(fun ff (exp, real) ->
+        Format.fprintf ff "\n\n%s\n%!" formatter;
+        print_z3_model ff solver exp real)
+    expected result;
+  end_time -. start_time
+
+let i32 n = Bil.int (Word.of_int ~width:32 n)
+
+let rec nest_ifs (var_gen : Env.var_gen) (level : int) (deepest_stmt : stmt list)
+  : stmt list =
+  if level = 0 then
+    deepest_stmt
+  else begin
+    let v = Var.create (Env.get_fresh var_gen) reg32_t in
+    let cond = Bil.(var v = i32 level) in
+    let stmt = nest_ifs var_gen (level - 1) deepest_stmt in
+    [ Bil.if_ (cond)
+        stmt
+        []
+    ]
+  end
+
+let branch_info (sub : Sub.t) : (Tid.t * Exp.t) Seq.t =
+  let jumps =
+    Term.enum blk_t sub
+    |> Seq.map ~f:(fun b -> Term.enum jmp_t b)
+    |> Seq.concat
+  in
+  let conditional_jumps = Seq.filter jumps
+      ~f:(fun j -> not (Exp.equal (Jmp.cond j) (Bil.int @@ Word.one 1))) in
+  Seq.map conditional_jumps ~f:(fun j -> (Term.tid j, Jmp.cond j))
+
+let cond_to_z3 (cond : Exp.t) (env : Env.t) : Env.z3_expr =
+  Pre.bv_to_bool (mk_z3_expr cond env) (Env.get_context env) 1
+
+let test_nested_ifs (test_ctx : test_ctxt) : unit =
+  let depth = 1000 in
+  let time_with_hooks =
+    let ctx = Env.mk_ctx () in
+    let var_gen = Env.mk_var_gen () in
+    let sub = nest_ifs var_gen depth [] |> bil_to_sub in
+    let jmp_spec = fun env post tid jmp ->
+      let branch_pre =
+        match Jmp.kind jmp with
+        | Goto (Direct tid) -> Option.value (Env.get_precondition env tid) ~default:post
+        | _ -> assert false
+      in
+      Seq.find_mapi (branch_info sub)
+        ~f:(fun i (t, cond) ->
+            if t = tid then begin
+              (* The first branch in the seq is the innermost in the nest. *)
+              if i = 0 then
+                Some (Bool.mk_ite ctx (cond_to_z3 cond env)
+                        (Bool.mk_false ctx) (Bool.mk_true ctx))
+              else
+                Some (Bool.mk_ite ctx (cond_to_z3 cond env)
+                        branch_pre (Bool.mk_true ctx))
+            end
+            else
+              None)
+    in
+    let env = Pre.mk_default_env ctx var_gen ~jmp_spec ~subs:(Seq.singleton sub) in
+    let post  = Bool.mk_true ctx in
+    let pre, _ = Pre.visit_sub env post sub in
+    let fmtr = format_log_error (Sub.to_string sub) pre post in
+    time_z3_result test_ctx ctx fmtr pre Z3.Solver.SATISFIABLE
+  in
+
+  let time_without_hooks =
+    let ctx = Env.mk_ctx () in
+    let var_gen = Env.mk_var_gen () in
+    let assert_sub, assert_expr = Bil_to_bir.mk_assert_fail () in
+    let sub = nest_ifs var_gen depth [Bil.jmp assert_expr] |> bil_to_sub in
+    let env = Pre.mk_default_env ctx var_gen ~subs:(Seq.of_list [sub; assert_sub]) in
+    let post  = Bool.mk_true ctx in
+    let pre, _ = Pre.visit_sub env post sub in
+    let fmtr = format_log_error (Sub.to_string sub) pre post in
+    time_z3_result test_ctx ctx fmtr pre Z3.Solver.SATISFIABLE
+  in
+  Printf.printf "\nTime with hooks:    %f\nTime without hooks: %f\n%!"
+    time_with_hooks time_without_hooks
+
+let suite = [
+  "Test branches" >: TestCase (Long, test_nested_ifs);
+]
