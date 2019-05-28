e3caebb710440e3d27df3bd13660a16df14117c0
Chloe Fortuna <cfortuna@draper.com>
2019-05-28 21:16:17 +0000

    Update year in copyright

--- wp/plugin/save_project/save_project.ml
+++ wp/plugin/save_project/save_project.ml
@@ -1,10 +1,23 @@
+(***************************************************************************)
+(*                                                                         *)
+(*  Copyright (C) 2018/2019 The Charles Stark Draper Laboratory, Inc.      *)
+(*                                                                         *)
+(*  This file is provided under the license found in the LICENSE file in   *)
+(*  the top-level directory of this project.                               *)
+(*                                                                         *)
+(*  This work is funded in part by ONR/NAWC Contract N6833518C0107.  Its   *)
+(*  content does not necessarily reflect the position or policy of the US  *)
+(*  Government and no official endorsement should be inferred.             *)
+(*                                                                         *)
+(***************************************************************************)
+
 open !Core_kernel
 open Bap.Std
 include Self()
 
 
 let main nm proj : unit =
-  let dest = 
+  let dest =
     match (nm, Project.get proj filename) with
     | ("",None) -> printf "Please specify a destination filename with --save-project-filename.\n";
                    exit 1;
@@ -16,10 +29,10 @@ let main nm proj : unit =
 module Cmdline = struct
   open Config
   let filename = param string "filename" ~doc:"Optional name of output file"
-                 
+
   let () = when_ready (fun {get=(!!)} ->
                Project.register_pass' (main !!filename))
-             
+
   let () = manpage [
      `S "DESCRIPTION";
      `P "Saves a binary's project data structure to disk."
