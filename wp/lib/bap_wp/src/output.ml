4effa40a5b013fad7a6ee9dfefecedc24ccc5770
Cody Roux <croux@draper.com>
2019-09-30 13:46:01 -0400

    Fix warnings in GDB output

--- wp/lib/bap_wp/src/output.ml
+++ wp/lib/bap_wp/src/output.ml
@@ -30,13 +30,6 @@ module Constr = Constraint
 open Core_kernel
 open Bap.Std
 
-type sign = Unsigned | Signed
-
-let string_of_sign s =
-  match s with
-  | Unsigned -> "u"
-  | Signed -> "s"
-
 exception NoValue of string
 
 let no_value_msg e m =
@@ -44,8 +37,8 @@ let no_value_msg e m =
   let m_str = Z3.Model.to_string m in
   Printf.sprintf "No value for symbol '%s' found in model: %s" e_str m_str
 
-let word_of_str s w sign =
-  let w_str = Printf.sprintf "%s:%d%s" s w (string_of_sign sign) in
+let word_of_str s w =
+  let w_str = Printf.sprintf "%s:%du" s w in
   Word.of_string w_str
 
 let z3_val_of e m =
@@ -55,9 +48,8 @@ let z3_val_of e m =
 
 let word_of_z3_bv bv =
   let bits = Z3.BitVector.get_size (Z3.Expr.get_sort bv) in
-  let sign = Unsigned in
   let s = Z3.Expr.to_string bv in
-  word_of_str (String.substr_replace_first s ~pattern:"#" ~with_:"0") bits sign
+  word_of_str (String.substr_replace_first s ~pattern:"#" ~with_:"0") bits
 
 let val_for_reg r m env =
   let reg_var = Var.create r (Type.imm 64) in
@@ -76,7 +68,7 @@ let strip_final_digits s =
         | '0' .. '9' -> pop_digits xs
         | _ -> l
       end
-    in 
+  in 
   let s'' = List.rev (pop_digits s') in
   String.of_char_list s''
 
@@ -85,7 +77,7 @@ let reg_names m =
   Z3.Model.get_decls |>
   List.map ~f:Z3.FuncDecl.get_name |>
   List.map ~f:Z3.Symbol.to_string |>
-  List.filter ~f:(fun s -> String.is_substring_at s 0 "R") |>
+  List.filter ~f:(fun s -> String.is_substring_at s ~pos:0 ~substring:"R") |>
   List.map ~f:strip_final_digits
 
 
@@ -108,19 +100,19 @@ let print_result (solver : Z3.Solver.solver) (status : Z3.Solver.status)
     breakpoint at the subroutine and fills the appropriate registers *)
 
 let output_gdb (solver : Z3.Solver.solver) (status : Z3.Solver.status)
-               (env : Env.t) ~func:(func : string) ~filename:(gdb_filename : string) : unit =
-    match status with
-    | Z3.Solver.SATISFIABLE ->
-        let model = Z3.Solver.get_model solver
+    (env : Env.t) ~func:(func : string) ~filename:(gdb_filename : string) : unit =
+  match status with
+  | Z3.Solver.SATISFIABLE ->
+    let model = Z3.Solver.get_model solver
                 |> Option.value_exn ?here:None ?error:None ?message:None in
-        let regs = reg_names model in
-        Out_channel.with_file gdb_filename  ~f:(fun t -> 
-            Printf.fprintf t "break *%s\n" func; (* The "*" is necessary to break before some slight setup *)
-            Printf.fprintf t "start\n";
-            List.iter regs ~f:(fun r -> 
-                  let w = val_for_reg r model env in
-                  let s = Format.asprintf "set $%s = %a \n" (String.lowercase r) Bap.Std.Word.pp_hex w  in
-                  Printf.fprintf t "%s" s))   
-    | _ -> ()
+    let regs = reg_names model in
+    Out_channel.with_file gdb_filename  ~f:(fun t -> 
+        Printf.fprintf t "break *%s\n" func; (* The "*" is necessary to break before some slight setup *)
+        Printf.fprintf t "start\n";
+        List.iter regs ~f:(fun r -> 
+            let w = val_for_reg r model env in
+            let s = Format.asprintf "set $%s = %a \n" (String.lowercase r) Bap.Std.Word.pp_hex w  in
+            Printf.fprintf t "%s" s))   
+  | _ -> ()
 
 
