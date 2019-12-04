79086de70060bd922e95d561898d8876ad8d4add
Chloe Fortuna <cfortuna@draper.com>
2019-12-04 22:11:50 +0000

    Add examples that test memory models

---
+++ wp/resources/sample_binaries/memory_samples/diff_data/run_wp.sh
@@ -0,0 +1,18 @@
+set -x
+
+dummy_dir=../../dummy
+
+compile () {
+  make
+}
+
+run () {
+  bap $dummy_dir/hello_world.out --pass=wp \
+    --wp-compare=true \
+    --wp-file1=main_1.bpj \
+    --wp-file2=main_2.bpj \
+    --wp-function=main \
+    --wp-gdb-filename=diff_data.gdb
+}
+
+compile && run
