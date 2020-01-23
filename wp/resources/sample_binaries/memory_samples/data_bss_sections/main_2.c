8c3a33f2609e9f3cf8d4508e790931326e33d611
Chloe Fortuna <cfortuna@draper.com>
2020-01-23 19:39:45 +0000

    Add test for matching data and bss sections for comparison

---
+++ wp/resources/sample_binaries/memory_samples/data_bss_sections/main_2.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+int y;
+int a;
+int x = 1;
+int b;
+int z = 2;
+
+int main(void) {
+
+  return x + y;
+
+}
---
+++ wp/resources/sample_binaries/memory_samples/data_bss_sections/run_wp.sh
@@ -0,0 +1,18 @@
+set -x
+
+dummy_dir=../../dummy
+
+compile () {
+  make
+}
+
+run () {
+  bap $dummy_dir/hello_world.out --pass=wp \
+    --wp-compare=true \
+    --wp-file1=main_1.bpj \
+    --wp-file2=main_2.bpj \
+    --wp-output-vars=RAX,RBX,RSP,RBP,R12,R13,R14,R15
+
+}
+
+compile && run
