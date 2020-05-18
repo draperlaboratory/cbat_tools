64d5c698840adbff75208b03b9906802cbf410ec
Chloe Fortuna <cfortuna@draper.com>
2020-05-18 19:01:01 +0000

    Refactor bil_to_bir

--- wp/lib/bap_wp/src/bil_to_bir.ml
+++ wp/lib/bap_wp/src/bil_to_bir.ml
@@ -30,80 +30,90 @@ let mk_assert_fail () : Sub.t * Exp.t =
   let call_sub = Sub.create ~tid:call_tid ~name:"__assert_fail" () in
   call_sub, Bil.unknown (Tid.to_string call_tid) reg64_t
 
-
-let rec stmt_to_blks (stmt : Bil.Types.stmt)
+let rec stmt_to_blks
+    (stmt : Bil.Types.stmt)
     (sub : Sub.Builder.t)
     (tail_blk : Blk.Builder.t)
   : Sub.Builder.t * Blk.Builder.t =
   match stmt with
-  | Bil.Types.Move (x, e) ->
-    Blk.Builder.add_def tail_blk (Def.create x e);
-    sub, tail_blk
-  | Bil.Types.If (e, ls, rs) ->
-    let tid_l = Tid.create () in
-    let tid_r = Tid.create () in
-    let sub_l, blk_l = bil_to_blks ls sub (Blk.Builder.create ~tid:tid_l ()) in
-    (* We pass along the sub builder, to add all required blocks in both branches *)
-    let sub_r, blk_r = bil_to_blks rs sub_l (Blk.Builder.create ~tid:tid_r ()) in
-    Blk.Builder.add_jmp tail_blk (Jmp.create ~cond:e (Goto (Label.direct tid_l)));
-    Blk.Builder.add_jmp tail_blk (Jmp.create (Goto (Label.direct tid_r)));
-    let old_blk = Blk.Builder.result tail_blk in
-    Sub.Builder.add_blk sub_r old_blk;
-    let merge_tid = Tid.create () in
-    let merge_blk = Blk.Builder.create ~tid:merge_tid () in
-    let lab = Label.direct merge_tid in
-    Blk.Builder.add_jmp blk_l (Jmp.create (Goto lab));
-    let lhs_blk = Blk.Builder.result blk_l in
-    Sub.Builder.add_blk sub_r lhs_blk;
-    Blk.Builder.add_jmp blk_r (Jmp.create (Goto lab));
-    let rhs_blk = Blk.Builder.result blk_r in
-    Sub.Builder.add_blk sub_r rhs_blk;
-    sub_r, merge_blk
-
-  | Bil.Types.While (e, ss) ->
-    let exit_tid = Tid.create () in
-    let entry_tid = Tid.create () in
-    let body_tid = Tid.create () in
-    let exit_block = Blk.Builder.create ~tid:exit_tid () in
-    let entry_block = Blk.Builder.create ~tid:entry_tid () in
-    let body_block = Blk.Builder.create ~tid:body_tid () in
-    Blk.Builder.add_jmp tail_blk (Jmp.create (Goto (Label.direct entry_tid)));
-    Sub.Builder.add_blk sub (Blk.Builder.result tail_blk);
-    Blk.Builder.add_jmp entry_block
-      (Jmp.create ~cond:(Bil.lnot e) (Goto (Label.direct exit_tid)));
-    Blk.Builder.add_jmp entry_block
-      (Jmp.create (Goto (Label.direct body_tid)));
-    Sub.Builder.add_blk sub (Blk.Builder.result entry_block);
-    let sub_loop, blk_loop = bil_to_blks ss sub body_block in
-    Blk.Builder.add_jmp blk_loop
-      (Jmp.create (Goto (Label.direct entry_tid)));
-    Sub.Builder.add_blk sub_loop (Blk.Builder.result blk_loop);
-    sub_loop, exit_block
-
-  | Bil.Types.Jmp e ->
-    begin
-      match e with
-      (* This is a huge hack to enable calling "special" functions with chosen names
-         and [tid]s
-      *)
-      | Bil.Unknown (tid_s, _) ->
-        let call_tid = Tid.from_string_exn tid_s in
-        Blk.Builder.add_jmp tail_blk
-          (Jmp.create (Call (Call.create ~target:(Label.direct call_tid) ())));
-        sub, tail_blk
-      | _ ->
-        Blk.Builder.add_jmp tail_blk
-          (Jmp.create (Goto (Label.indirect e)));
-        sub, tail_blk
-    end
-
-  (* We currently don't handle any of these cases *)
-
+  | Bil.Types.Move (x, e) -> mk_move x e sub tail_blk
+  | Bil.Types.If (e, ls, rs) -> mk_if e ls rs sub tail_blk
+  | Bil.Types.While (e, ss) -> mk_while e ss sub tail_blk
+  | Bil.Types.Jmp e -> mk_jmp e sub tail_blk
+  (* We currently don't handle any of these cases. *)
   | Bil.Types.Special _ -> assert false
-
   | Bil.Types.CpuExn _ -> assert false
 
-and bil_to_blks (stmts : Bil.t)
+and mk_move (x : Var.t) (e : Exp.t) (sub : Sub.Builder.t) (blk : Blk.Builder.t)
+  : Sub.Builder.t * Blk.Builder.t  =
+  Blk.Builder.add_def blk (Def.create x e);
+  sub, blk
+
+and mk_if
+    (e : Exp.t)
+    (ls : stmt list)
+    (rs : stmt list)
+    (sub : Sub.Builder.t)
+    (blk : Blk.Builder.t)
+  : Sub.Builder.t * Blk.Builder.t =
+  let tid_l = Tid.create () in
+  let tid_r = Tid.create () in
+  let sub_l, blk_l = bil_to_blks ls sub (Blk.Builder.create ~tid:tid_l ()) in
+  (* We pass along the sub builder, to add all required blocks in both branches *)
+  let sub_r, blk_r = bil_to_blks rs sub_l (Blk.Builder.create ~tid:tid_r ()) in
+  Blk.Builder.add_jmp blk (Jmp.create ~cond:e (Goto (Label.direct tid_l)));
+  Blk.Builder.add_jmp blk (Jmp.create (Goto (Label.direct tid_r)));
+  let old_blk = Blk.Builder.result blk in
+  Sub.Builder.add_blk sub_r old_blk;
+  let merge_tid = Tid.create () in
+  let merge_blk = Blk.Builder.create ~tid:merge_tid () in
+  let lab = Label.direct merge_tid in
+  Blk.Builder.add_jmp blk_l (Jmp.create (Goto lab));
+  let lhs_blk = Blk.Builder.result blk_l in
+  Sub.Builder.add_blk sub_r lhs_blk;
+  Blk.Builder.add_jmp blk_r (Jmp.create (Goto lab));
+  let rhs_blk = Blk.Builder.result blk_r in
+  Sub.Builder.add_blk sub_r rhs_blk;
+  sub_r, merge_blk
+
+and mk_while (e : Exp.t) (ss : stmt list) (sub : Sub.Builder.t) (blk : Blk.Builder.t)
+  : Sub.Builder.t * Blk.Builder.t =
+  let exit_tid = Tid.create () in
+  let entry_tid = Tid.create () in
+  let body_tid = Tid.create () in
+  let exit_block = Blk.Builder.create ~tid:exit_tid () in
+  let entry_block = Blk.Builder.create ~tid:entry_tid () in
+  let body_block = Blk.Builder.create ~tid:body_tid () in
+  Blk.Builder.add_jmp blk (Jmp.create (Goto (Label.direct entry_tid)));
+  Sub.Builder.add_blk sub (Blk.Builder.result blk);
+  Blk.Builder.add_jmp entry_block
+    (Jmp.create ~cond:(Bil.lnot e) (Goto (Label.direct exit_tid)));
+  Blk.Builder.add_jmp entry_block
+    (Jmp.create (Goto (Label.direct body_tid)));
+  Sub.Builder.add_blk sub (Blk.Builder.result entry_block);
+  let sub_loop, blk_loop = bil_to_blks ss sub body_block in
+  Blk.Builder.add_jmp blk_loop
+    (Jmp.create (Goto (Label.direct entry_tid)));
+  Sub.Builder.add_blk sub_loop (Blk.Builder.result blk_loop);
+  sub_loop, exit_block
+
+(* This is a huge hack to enable calling "special" functions with chosen names
+   and [tid]s. *)
+and mk_jmp (e : Exp.t) (sub : Sub.Builder.t) (blk : Blk.Builder.t)
+  : Sub.Builder.t * Blk.Builder.t =
+  match e with
+  | Bil.Unknown (tid_s, _) ->
+    let call_tid = Tid.from_string_exn tid_s in
+    Blk.Builder.add_jmp blk
+      (Jmp.create (Call (Call.create ~target:(Label.direct call_tid) ())));
+    sub, blk
+  | _ ->
+    Blk.Builder.add_jmp blk
+      (Jmp.create (Goto (Label.indirect e)));
+    sub, blk
+
+and bil_to_blks
+    (stmts : Bil.t)
     (sub : Sub.Builder.t)
     (tail_blk : Blk.Builder.t)
   : Sub.Builder.t * Blk.Builder.t =
