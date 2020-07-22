cfdbd3db6cf4599fd5bd5d2bae0b031302c65d16
Chloe Fortuna <cfortuna@draper.com>
2020-07-22 18:00:29 +0000

    Add test for writing smtlib postcondition for comparing post registers

---
+++ wp/resources/sample_binaries/same_signs/main_2.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+bool same_signs(int x, int y) {
+    return !((x ^ y) < 0);
+}
+
+int main() {
+    // Try it out
+    int x = 10;
+    int y = -10;
+    fputs(same_signs(x, y) ? "true\n" : "false\n", stdout);
+    return 0;
+}
