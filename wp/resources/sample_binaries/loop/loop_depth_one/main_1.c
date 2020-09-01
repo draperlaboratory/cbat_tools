0e2a09f2ef81c3256eec689ded4d72b8065a7bae
Justin Restivo <justin.p.restivo@gmail.com>
2020-09-01 16:20:38 -0400

    Added single loop tests

---
+++ wp/resources/sample_binaries/loop/loop_depth_one/main_1.c
@@ -0,0 +1,16 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+int foo(uint8_t i){
+  int k = 0;
+  while(k < 3){
+    k++;
+  }
+  return i;
+}
+
+int main(int argc, char** argv) {
+  return 0;
+}
---
+++ wp/resources/sample_binaries/loop/loop_depth_one/run_wp_less_loop.sh
@@ -0,0 +1,15 @@
+# in the case we do not unroll the loop all the way and terminate early
+# expected behavior is to not be affected by the loop. See issue #236
+
+# Should return SAT but currently returns UNSAT; bug
+
+run() {
+  bap wp \
+    --func=foo \
+    --num-unroll=2 \
+    --postcond="(assert (not (= RAX #x0000000000000005)))" \
+    --no-byteweight \
+    -- main_1
+}
+
+run
