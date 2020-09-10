1d3900919ded3ad33aafb428836a2f6101b1ead0
Justin Restivo <justin.p.restivo@gmail.com>
2020-09-10 11:11:04 -0400

    Added docs, updated test makefiles

--- wp/resources/sample_binaries/pointer_flag/run_wp_sat.sh
+++ wp/resources/sample_binaries/pointer_flag/run_wp_sat.sh
@@ -4,20 +4,20 @@
 # value between functions. The abscence of the flag should allow the pointer
 # argument in RDI to be used to point to this return address, as it lives
 # on the uninitialized portion of the stack at the start of the function.
- 
+
 # Should return SAT
 
 set -x
 
 run_sat () {
   bap wp \
-          --func=deref \
-          --num-unroll=0 \
-          --no-byteweight \
-          --mem-offset \
-          --show=bir,refuted-goals,paths \
-          --compare-post-reg-values=R12,R13,R14,R15,RBX,RSP,RBP,RAX \
-          -- main_1 main_2
-  }
+    --func=deref \
+    --num-unroll=0 \
+    --no-byteweight \
+    --mem-offset \
+    --show=bir,refuted-goals,paths \
+    --compare-post-reg-values=R12,R13,R14,R15,RBX,RSP,RBP,RAX \
+    -- main_1 main_2
+}
 
 run_sat
