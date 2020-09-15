b19494ff392fbaa3c259120001d7513fe87c91c6
Justin Restivo <justin.p.restivo@gmail.com>
2020-09-15 17:40:25 -0400

    Make column width generally under 80 characters

--- wp/plugin/tests/test_wp.ml
+++ wp/plugin/tests/test_wp.ml
@@ -75,8 +75,8 @@ let get_reg_from_line (line : string) (regs_list : StringSet.t) :
 (* Look for a line containing SAT!, UNSAT!, or UNKNOWN! in
    plugin output and compare with expected *)
 let check_result (stream : char Stream.t) (expected_result : string)
-    (regs_list : StringSet.t) (* set of registers to look for values of *)
-    (checker_wrapped: ((string StringMap.t) -> string option) option) (* results -> "is this valid?" *)
+    (regs_list : StringSet.t)
+    (checker_wrapped: ((string StringMap.t) -> string option) option)
     (ctxt : test_ctxt) : unit =
   let buff = Buffer.create 16 in
   let results = StringMap.empty in
@@ -123,11 +123,15 @@ let check_list (expected_reg_models : ((string * string) list) list )
               begin match StringMap.find observed_mapping reg with
                 (* if register not observed, then fail *)
                 | None ->
-                  Some (Printf.sprintf "\n\tRegister %s not found in returned model.\n" reg)
+                  Printf.sprintf
+                    "\n\tRegister %s not found in returned model.\n"
+                    reg |> Some
                 | Some observed_value ->
                   if observed_value = value then None
                   else
-                    Some (Printf.sprintf "\tRegister mismatch: %s expected to be %s but was %s\n" reg value observed_value )
+                    Printf.sprintf
+                      "\tRegister mismatch: %s expected to be %s but was %s\n"
+                      reg value observed_value |> Some
               end in
             Option.merge ~f:(^) acc err
           ) in
@@ -145,7 +149,6 @@ let check_list (expected_reg_models : ((string * string) list) list )
 let bv_of_string ?bits:(bits=64) (s : string) : Bitvector.t =
   s |> int_of_string |> Bitvector.of_int ~width:bits
 
-
 let gather_register_values (register_names : string list) (mapping: string StringMap.t)
   : Bitvector.t list =
   register_names |> List.map ~f:(fun name ->
@@ -154,7 +157,9 @@ let gather_register_values (register_names : string list) (mapping: string Strin
     )
 
 let get_element (board_list : Bitvector.t list) (mask_width : int) (index : int): int =
-  let mask = List.range ~stride:1 ~start:`inclusive ~stop:`exclusive 0 mask_width |> List.fold ~init:(0) ~f:(fun acc i -> acc + (1 lsl i)) |> Bitvector.of_int ~width:64 in
+  let mask = List.range ~stride:1 ~start:`inclusive ~stop:`exclusive 0 mask_width
+             |> List.fold ~init:(0) ~f:(fun acc i -> acc + (1 lsl i))
+             |> Bitvector.of_int ~width:64 in
   let bits_to_shift = index * mask_width in
   let shift_distance_int = (bits_to_shift) / 64 in
   let board = List.nth_exn board_list shift_distance_int in
@@ -186,7 +191,8 @@ let check_n_queen_diag_col_row (n : int) (board: Bitvector.t list) : string opti
               let result = List.append first second in
               List.zip_exn acc_inner result |> List.map ~f:(fun (l, r) -> l + r)) in
       let gen_err = fun i v t ->
-        if (v = 1 && i < 2) || (v <= 1 && 2 <= i) then None else Printf.sprintf "\n%s %d incorrect\n" t i |> Some in
+        if (v = 1 && i < 2) || (v <= 1 && 2 <= i) then None
+        else Printf.sprintf "\n%s %d incorrect\n" t i |> Some in
       let err_types = ["row"; "col"; "right diagonal lower";
                        "right diagonal upper"; "left diagonal lower";
                        "left diagonal upper"] in
@@ -217,10 +223,14 @@ let check_n_queens (n: int) : (string StringMap.t -> string option) =
 
 let check_for_all (indices : int list) (board : Bitvector.t) : string option =
   let expected_values = IntSet.of_list [0; 1; 2; 3] in
-  let actual_values = List.map indices ~f:(fun x -> get_element [board] 2 x) |> IntSet.of_list in
-  if (IntSet.inter expected_values actual_values |> IntSet.length) = (IntSet.length expected_values)
+  let actual_values =
+    List.map indices ~f:(fun x -> get_element [board] 2 x) |> IntSet.of_list in
+  if
+    (IntSet.inter expected_values actual_values |> IntSet.length)
+    = (IntSet.length expected_values)
   then None else
-    let indx_str = List.fold indices ~init:("") ~f:(fun acc x -> acc ^ "," ^ (string_of_int x)) in
+    let indx_str =
+      List.fold indices ~init:("") ~f:(fun acc x -> acc ^ "," ^ (string_of_int x)) in
     Printf.sprintf "\n indices %s did not match what was expected" indx_str |> Some
 
 
@@ -231,7 +241,8 @@ let check_two_by_two_sudoku (var_mapping : string StringMap.t) : string option =
     [[0; 1; 2; 3]; [4; 5; 6; 7]; [8; 9; 10; 11]; [12; 13; 14; 15];
      [0; 4; 8; 12]; [1; 5; 9; 13]; [2; 6; 10; 14]; [3; 7; 11; 15];
      [0; 1; 4; 5]; [2; 3; 6; 7;]; [8; 9; 12; 13]; [10; 11; 14; 15];] in
-  List.fold snippets ~init:(None) ~f:(fun acc l -> Option.merge acc (check_for_all l sudoku_board) ~f:(^))
+  List.fold snippets ~init:(None)
+    ~f:(fun acc l -> Option.merge acc (check_for_all l sudoku_board) ~f:(^))
 
 let bad_hash_function (c : Bitvector.t) (cur_hash : Bitvector.t) =
   let t_1 = Bitvector.lshift cur_hash ("0x6" |> bv_of_string ~bits:32) in
@@ -245,11 +256,13 @@ let check_bad_hash_function (registers : string list) : ((string StringMap.t) ->
   (* index of the special character *)
   let result_index = "0xf" |> bv_of_string ~bits:32 in
   let result = gather_register_values registers var_mapping
-               |> List.fold ~init:("0x0" |> bv_of_string ~bits:32) ~f:(fun acc v -> bad_hash_function v acc) in
+               |> List.fold ~init:("0x0" |> bv_of_string ~bits:32)
+                 ~f:(fun acc v -> bad_hash_function v acc) in
   if result = result_index then
     None
   else
-    Printf.sprintf "\n Expected the hash %s but got hash %s" (Bitvector.to_string result) (Bitvector.to_string result_index)
+    Printf.sprintf "\n Expected the hash %s but got hash %s"
+      (Bitvector.to_string result) (Bitvector.to_string result_index)
     |> Some
 
 (* The length of a test_case is in seconds.
@@ -284,18 +297,14 @@ let test_skip (msg : string) (_ : test) (_ : test_ctxt) : unit =
 let lift_out_regs (reg_value : ((string * string) list) list) : StringSet.t =
   List.fold reg_value ~init:(StringSet.empty) ~f:(fun acc model ->
       List.fold model ~init:(StringSet.empty) ~f:(fun acc (reg, _) ->
-          StringSet.add acc reg
-        )
-      |> StringSet.union acc
-
-    )
-(* StringSet.empty *)
+          StringSet.add acc reg) |> StringSet.union acc)
 
 let suite = [
 
   "Equiv argc"                     >: (
     let models = [ [("RDI", "0x0000000000000002")]; ] in
-    test_plugin "equiv_argc" sat ~reg_list:(lift_out_regs models) ~checker:(check_list models |> Some)
+    test_plugin "equiv_argc" sat
+      ~reg_list:(lift_out_regs models) ~checker:(check_list models |> Some)
   );
 
   (* Test elf comparison *)
@@ -316,17 +325,22 @@ let suite = [
     test_plugin "equiv_argc" sat ~script:"run_wp_force.sh"
       ~reg_list:(lift_out_regs models) ~checker:(check_list models |> Some));
 
-  "Precondition: disallow 2"       >: test_plugin "equiv_argc" unsat ~script:"run_wp_disallow.sh";
+  "Precondition: disallow 2"       >: test_plugin "equiv_argc" unsat
+    ~script:"run_wp_disallow.sh";
 
   "Equiv null check"               >: test_plugin "equiv_null_check" sat;
 
   "Init var compare: UNSAT"        >: test_plugin "init_var_compare" unsat;
-  "Init var compare: SAT"          >: test_plugin "init_var_compare" sat ~script:"run_wp_sat.sh";
+  "Init var compare: SAT"          >: test_plugin "init_var_compare" sat
+    ~script:"run_wp_sat.sh";
 
   "Arrays in data section"         >: test_plugin "memory_samples/arrays" sat;
-  "Arrays in data section"         >: test_plugin "memory_samples/arrays" sat ~script:"run_wp_mem_offset.sh";
-  "Arrays in data section"         >: test_plugin "memory_samples/arrays" sat ~script:"run_wp_pre.sh";
-  "Arrays in data section"         >: test_plugin "memory_samples/arrays" unsat ~script:"run_wp_pre_mem_offset.sh";
+  "Arrays in data section"         >: test_plugin "memory_samples/arrays" sat
+    ~script:"run_wp_mem_offset.sh";
+  "Arrays in data section"         >: test_plugin "memory_samples/arrays" sat
+    ~script:"run_wp_pre.sh";
+  "Arrays in data section"         >: test_plugin "memory_samples/arrays" unsat
+    ~script:"run_wp_pre_mem_offset.sh";
 
   "Data/BSS sections"              >: test_plugin "memory_samples/data_bss_sections" sat;
   "Data/BSS sections"              >: test_plugin "memory_samples/data_bss_sections" unsat
@@ -344,24 +358,29 @@ let suite = [
 
   "No position independent"        >: test_plugin "no_position_independent" sat;
 
-  "No stack protection"            >: test_plugin "no_stack_protection" sat ~length:Short;
+  "No stack protection"            >: test_plugin "no_stack_protection" sat
+    ~length:Short;
 
   "Null dereference: no check"     >: test_plugin "non_null_check" unsat;
-  "Null dereference: with check"   >: test_plugin "non_null_check" sat ~script:"run_wp_null_deref.sh";
+  "Null dereference: with check"   >: test_plugin "non_null_check" sat
+    ~script:"run_wp_null_deref.sh";
 
   "Pointer input"                  >: test_plugin "pointer_input" unsat;
 
   "Position independent"           >: test_plugin "position_independent" sat;
 
   "Retrowrite stub: pop RSP"       >: test_plugin "retrowrite_stub" unsat;
-  "Retrowrite stub: inline AFL"    >: test_plugin "retrowrite_stub" unsat ~script:"run_wp_inline_afl.sh";
-  "Retrowrite stub: inline all"    >: test_plugin "retrowrite_stub" unsat ~script:"run_wp_inline_all.sh";
+  "Retrowrite stub: inline AFL"    >: test_plugin "retrowrite_stub" unsat
+    ~script:"run_wp_inline_afl.sh";
+  "Retrowrite stub: inline all"    >: test_plugin "retrowrite_stub" unsat
+    ~script:"run_wp_inline_all.sh";
   "Retrowrite stub no ret in call" >: test_plugin "retrowrite_stub_no_ret" unsat;
 
   "ROP example"                    >:: test_skip fail_msg (test_plugin "rop_example" unsat);
 
   "Same signs: post registers"     >: test_plugin "same_signs" unsat;
-  "Same signs: postcondition"      >: test_plugin "same_signs" unsat ~script:"run_wp_postcond.sh";
+  "Same signs: postcondition"      >: test_plugin "same_signs" unsat
+    ~script:"run_wp_postcond.sh";
 
   "Switch case assignments"        >: (
     let models = [ [("RDI", "0x0000000000000000")]; ] in
@@ -385,52 +404,65 @@ let suite = [
 
   "Function call"                  >: (
     let models = [ [("RDI", "0x0000000000000005")]; ] in
-    test_plugin "function_call" sat ~script:"run_wp_inline_foo.sh" ~reg_list:(lift_out_regs models)
+    test_plugin "function_call" sat
+      ~script:"run_wp_inline_foo.sh" ~reg_list:(lift_out_regs models)
       ~checker:(check_list models |> Some)
   );
   "Function call: inline all"      >: (
     let models = [ [("RDI", "0x0000000000000005")]; ] in
-    test_plugin "function_call" sat ~script:"run_wp_inline_all.sh" ~reg_list:(lift_out_regs models)
+    test_plugin "function_call" sat
+      ~script:"run_wp_inline_all.sh" ~reg_list:(lift_out_regs models)
       ~checker:(check_list models |> Some)
   );
 
 
   "Function spec: no inlining"     >: test_plugin "function_spec" sat;
-  "Function spec: inline foo"      >: test_plugin "function_spec" unsat ~script:"run_wp_inline_foo.sh";
-  "Function spec: inline all "     >: test_plugin "function_spec" unsat ~script:"run_wp_inline_all.sh";
-  "Function spec: inline garbage"  >: test_plugin "function_spec" sat ~script:"run_wp_inline_garbage.sh";
+  "Function spec: inline foo"      >: test_plugin "function_spec" unsat
+    ~script:"run_wp_inline_foo.sh";
+  "Function spec: inline all "     >: test_plugin "function_spec" unsat
+    ~script:"run_wp_inline_all.sh";
+  "Function spec: inline garbage"  >: test_plugin "function_spec" sat
+    ~script:"run_wp_inline_garbage.sh";
 
   "Goto string"                    >: test_plugin "goto_string" sat;
-  "Goto string: inline all"        >: test_plugin "goto_string" sat ~script:"run_wp_inline.sh";
+  "Goto string: inline all"        >: test_plugin "goto_string" sat
+    ~script:"run_wp_inline.sh";
 
   "Init var value in post: UNSAT:" >: test_plugin "init_var" unsat;
-  "Init var value in post: SAT"    >: test_plugin "init_var" sat ~script:"run_wp_sat.sh";
+  "Init var value in post: SAT"    >: test_plugin "init_var" sat
+    ~script:"run_wp_sat.sh";
 
   "Linked list: no mem check"      >: test_plugin "linked_list" unsat;
-  "Linked list: with mem check"    >: test_plugin "linked_list" sat ~script:"run_wp_null_deref.sh";
+  "Linked list: with mem check"    >: test_plugin "linked_list" sat
+    ~script:"run_wp_null_deref.sh";
 
   "Loop"                           >: test_plugin "loop" sat;
 
   "Nested function calls"               >: test_plugin "nested_function_calls" unsat;
   "Nested function calls: inline regex" >: (
     let models = [ [("RDI", "0x0000000000000004")]; ] in
-    test_plugin "nested_function_calls" sat ~script:"run_wp_inline_regex.sh" ~reg_list:(lift_out_regs models)
+    test_plugin "nested_function_calls" sat
+      ~script:"run_wp_inline_regex.sh" ~reg_list:(lift_out_regs models)
       ~checker:(check_list models |> Some)
   );
 
   "Nested function calls: inline all"   >: (
     let models = [ [("RDI", "0x0000000000000004")]; ] in
-    test_plugin "nested_function_calls" sat ~script:"run_wp_inline_all.sh" ~reg_list:(lift_out_regs models)
+    test_plugin "nested_function_calls" sat
+      ~script:"run_wp_inline_all.sh" ~reg_list:(lift_out_regs models)
       ~checker:(check_list models |> Some)
   );
 
 
   "Nested ifs"                     >: test_plugin "nested_ifs" unsat;
-  "Nested ifs: goto"               >: test_plugin "nested_ifs" unsat ~script:"run_wp_goto.sh";
-  "Nested ifs: inline"             >: test_plugin "nested_ifs" unsat ~script:"run_wp_inline.sh";
+  "Nested ifs: goto"               >: test_plugin "nested_ifs" unsat
+    ~script:"run_wp_goto.sh";
+  "Nested ifs: inline"             >: test_plugin "nested_ifs" unsat
+    ~script:"run_wp_inline.sh";
 
   "Null dereference: no check"     >: test_plugin "null_deref" unsat;
-  "Null dereference: with check"   >: test_plugin "null_deref" sat ~script:"run_wp_null_deref.sh";
+  "Null dereference: with check"   >: test_plugin "null_deref" sat
+    ~script:"run_wp_null_deref.sh";
 
   "User defined postcondition"     >: test_plugin "return_argc" sat;
 
@@ -440,20 +472,28 @@ let suite = [
       ~checker:(check_list models |> Some)
   );
 
-  "Simple WP: precondition"        >: test_plugin "simple_wp" unsat ~script:"run_wp_pre.sh";
+  "Simple WP: precondition"        >: test_plugin "simple_wp" unsat
+    ~script:"run_wp_pre.sh";
 
-  "Verifier assume SAT"            >: test_plugin "verifier_calls" sat ~script:"run_wp_assume_sat.sh";
-  "Verifier assume UNSAT"          >: test_plugin "verifier_calls" unsat ~script:"run_wp_assume_unsat.sh";
-  "Verifier nondent"               >: test_plugin "verifier_calls" sat ~script:"run_wp_nondet.sh";
+  "Verifier assume SAT"            >: test_plugin "verifier_calls" sat
+    ~script:"run_wp_assume_sat.sh";
+  "Verifier assume UNSAT"          >: test_plugin "verifier_calls" unsat
+    ~script:"run_wp_assume_unsat.sh";
+  "Verifier nondent"               >: test_plugin "verifier_calls" sat
+    ~script:"run_wp_nondet.sh";
 
   (* Test performance *)
 
-  "Debruijn: 8 bit"                >: test_plugin "debruijn" unsat ~script:"run_wp_8bit.sh";
-  "Debruijn: 16 bit"               >: test_plugin "debruijn" unsat ~script:"run_wp_16bit.sh";
-  "Debruijn: 32 bit"               >: test_plugin "debruijn" unsat ~script:"run_wp_32bit.sh";
+  "Debruijn: 8 bit"                >: test_plugin "debruijn" unsat
+    ~script:"run_wp_8bit.sh";
+  "Debruijn: 16 bit"               >: test_plugin "debruijn" unsat
+    ~script:"run_wp_16bit.sh";
+  "Debruijn: 32 bit"               >: test_plugin "debruijn" unsat
+    ~script:"run_wp_32bit.sh";
 
   "NQueens solver 4x4"             >: (
-    test_plugin "nqueens" sat ~reg_list:(["RDI"; "RSI"; "RDX"; "RCX"] |> StringSet.of_list)
+    test_plugin "nqueens" sat
+      ~reg_list:(["RDI"; "RSI"; "RDX"; "RCX"] |> StringSet.of_list)
       ~checker:(check_n_queens 4 |> Some)
   );
 
