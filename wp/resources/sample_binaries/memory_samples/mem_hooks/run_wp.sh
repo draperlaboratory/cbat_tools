03e1b1690f907705815875d431c3a0e368616e33
Chloe Fortuna <cfortuna@draper.com>
2020-11-05 20:18:57 +0000

    Add test for valid memory hooks

---
+++ wp/resources/sample_binaries/memory_samples/mem_hooks/run_wp.sh
@@ -0,0 +1,19 @@
+# Tests having different locations for the data section and same values on the
+# stack. The binaries are the same except for the location of val.
+
+# This tests the usage of the --check-invalid-derefs flag.
+# This test should have the same output whether this flag is set or not.
+
+# Should return SAT.
+
+run () {
+  bap wp \
+    --func=main \
+    --compare-post-reg-values=RAX \
+    --show=bir,paths \
+    --check-invalid-derefs \
+    --no-glibc-runtime \
+    -- main_1 main_2
+}
+
+run
