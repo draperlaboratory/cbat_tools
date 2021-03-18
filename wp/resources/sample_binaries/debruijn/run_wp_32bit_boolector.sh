f62c5c9dea6b0f38a76ee1f6b415ece95b02aef7
Philip Zucker <philzook58@gmail.com>
2021-03-18 17:22:27 -0400

    Some boolector integration tests

---
+++ wp/resources/sample_binaries/debruijn/run_wp_32bit_boolector.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# an example that compares the debruijn method of finding the LSB instead
+# to that of using a for loop
+
+# Should return UNSAT
+
+run () {
+  if type boolector
+  then
+  bap wp \
+    --func=rightmost_index_32 \
+    --compare-post-reg-values=RAX \
+    --no-byteweight \
+    --ext-solver=boolector \
+    -- ./bin/main_1 ./bin/main_2
+  else
+  bap wp \
+  --func=rightmost_index_32 \
+  --compare-post-reg-values=RAX \
+  --no-byteweight \
+  -- ./bin/main_1 ./bin/main_2
+  fi
+}
+
+run
