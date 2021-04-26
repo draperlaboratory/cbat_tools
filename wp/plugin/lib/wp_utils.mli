61efb676dd75a980e4673194d641b03f1532fa13
Cody Roux <croux@draper.com>
2021-04-26 17:48:08 -0400

    Remove dead code from wp_utils.

--- wp/plugin/lib/wp_utils.mli
+++ wp/plugin/lib/wp_utils.mli
@@ -20,14 +20,9 @@
 
 *)
 
-open Core_kernel
-
 open Bap_main
 open Bap.Std
-open Bap_wp
 open Bap_core_theory
-
-module Env = Environment
 
 (** The loader WP uses for lifting a binary, defaulting to LLVM. *)
 val loader : string
@@ -36,58 +31,3 @@ val loader : string
     given filepath using the BAP context and loader for lifting the binary. *)
 val read_program :
   ctxt -> loader:string -> filepath:string -> Program.t * Theory.target
-
-(** [find_func_err subs name] obtains a function named [name] from a
-    sequence of subroutines [subs]. Fails if the function can't be found. *)
-val find_func_err : Sub.t Seq.t -> string -> Sub.t
-
-(** [match_inline regex subs] finds a sequence of subroutines to inline from
-    [subs] that match with [regex]. *)
-val match_inline : string option -> Sub.t Seq.t -> Sub.t Seq.t
-
-(** Converts a set of variables to a string for printing debug logs. *)
-val varset_to_string : Var.Set.t -> string
-
-(** Updates the number of times to unroll a loop based on the user's input. *)
-val update_default_num_unroll : int option -> unit
-
-(** Returns a record that can be used when creating an {!Environment.t} to
-    change the default the stack base and size. *)
-val update_stack : base:int option -> size:int option -> Env.mem_range
-
-(** Checks if the user provided a filename to output a gdb script to, and if
-    provided, outputs the script. *)
-val output_to_gdb :
-  filename:string option
-  -> func:string
-  -> Z3.Solver.solver
-  -> Z3.Solver.status
-  -> Env.t
-  -> unit
-
-(** Checks if the user provided a filename to input a bildb init file to, and if
-    provided, outputs the script. *)
-val output_to_bildb :
-  filename:string option
-  -> Z3.Solver.solver
-  -> Z3.Solver.status
-  -> Env.t
-  -> unit
-
-(** [spec_of_name name] returns the function spec creator with the
-    corresponding [name]. *)
-val spec_of_name : string -> Sub.t -> Theory.target -> Env.fun_spec option
-
-(** [mk_func_name orig modif regex] creates a map of modified subroutine names
-    to original subroutine names based off the regex list. *)
-val mk_func_name_map :
-  orig:Sub.t Seq.t
-  -> modif:Sub.t Seq.t
-  -> (string * string) list
-  -> string String.Map.t
-
-(** [get_mod_func_name orig_name regex] determines the name of the modified
-    subroutine based off of the original subroutine's name and the regex list.
-    Raises an exception if a subroutine that matches the regex can't be
-    found. *)
-val get_mod_func_name : string -> (string * string) list -> string option
