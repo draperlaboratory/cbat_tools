72d7bb33bd1a8deb245dd7150304cac52ebd06e4
Cody Roux <croux@draper.com>
2021-08-11 16:34:39 -0400

    Add rodata info to the initial hypotheses of comparative and single analyses. Still needs: 1. a flag to either enforce or skip it 2. tests!

--- wp/lib/bap_wp/src/compare.ml
+++ wp/lib/bap_wp/src/compare.ml
@@ -23,6 +23,14 @@ module Env = Environment
 module Pre = Precondition
 module Constr = Constraint
 
+type 'a code =
+  {
+    prog : 'a term;
+    env : Env.t;
+    mem : value memmap;
+  }
+
+
 (* We return an updated pair of environments here, since if we are generating
    fresh variables, we want to keep those same fresh names in the analysis *)
 let set_to_eqs (env1 : Env.t) (env2 : Env.t) (vars : Var.Set.t) : Constr.t list * Env.t * Env.t =
@@ -57,8 +65,8 @@ let init_vars (env1 : Env.t) (env2 : Env.t) (vars : Var.Set.t)
 let compare_blocks
     ~pre_regs:(pre_regs : Var.Set.t)
     ~post_regs:(post_regs : Var.Set.t)
-    ~original:(blk1, env1 : Blk.t * Env.t)
-    ~modified:(blk2, env2 : Blk.t * Env.t)
+    ~original:({ prog=blk1; env=env1; _ } : blk code)
+    ~modified:({ prog=blk2; env=env2; _ } : blk code)
     ~smtlib_post:(smtlib_post : string)
     ~smtlib_hyp:(smtlib_hyp : string)
   : Constr.t * Env.t * Env.t =
@@ -77,21 +85,26 @@ let compare_blocks
 
 (* The type of functions that generate a postcondition or hypothesis for comparative
    analysis. Also updates the environments as needed. *)
-type comparator = original:(Sub.t * Env.t) -> modified:(Sub.t * Env.t) ->
+type comparator = original:sub code -> modified:sub code ->
   rename_set:Var.Set.t -> Constr.t * Env.t * Env.t
 
 let fold_comparators
-    ~original:(sub1, env1 : Sub.t * Env.t)
-    ~modified:(sub2, env2 : Sub.t * Env.t)
+    ~original:(code1 : sub code)
+    ~modified:(code2 : sub code)
     ~rename_set:(vars : Var.Set.t)
     (comparators : comparator list)
   : Constr.t * Env.t * Env.t =
+  let env1 = code1.env in
+  let env2 = code2.env in
   let ctx = Env.get_context env1 in
   let constrs, env1, env2 =
     List.fold comparators ~init:([], env1, env2)
       ~f:(fun (comps, env1, env2) comp ->
           let c, env1, env2 =
-            comp ~original:(sub1, env1) ~modified:(sub2, env2) ~rename_set:vars
+            comp
+              ~original:{ code1 with env=env1 }
+              ~modified:{ code2 with env=env2 }
+              ~rename_set:vars
           in
           (* We are not adding the trivial constraints to prevent blow up in
              the final precondition. *)
@@ -109,35 +122,40 @@ let fold_comparators
 let compare_subs
     ~postconds:(postconds : comparator list)
     ~hyps:(hyps : comparator list)
-    ~original:(sub1, env1 : Sub.t * Env.t)
-    ~modified:(sub2, env2 : Sub.t * Env.t)
+    ~original:(code1 : sub code)
+    ~modified:(code2 : sub code)
   : Constr.t * Env.t * Env.t =
-  let env2 = Env.set_freshen env2 true in
+  let env2 = Env.set_freshen code2.env true in
+  let code2 = { code2 with env = env2 } in
   let vars = Var.Set.union
-      (Pre.get_vars env1 sub1) (Pre.get_vars env2 sub2) in
+      (Pre.get_vars code1.env code1.prog) (Pre.get_vars code2.env code2.prog) in
   let post, env1, env2 =
-    fold_comparators postconds ~original:(sub1, env1) ~modified:(sub2, env2)
+    fold_comparators postconds
+      ~original:code1
+      ~modified:code2
       ~rename_set:vars
   in
   info "\nPostcondition:\n%s\n%!" (Constr.to_string post);
   let hyps, env1, env2 =
     (* We will always generate Z3 variables representing the initial value of
        registers in the subroutine. *)
-    let init_vars ~original:(_, env1) ~modified:(_, env2) ~rename_set:vars =
-      let vars, env1, env2 = init_vars env1 env2 vars in
+    let init_vars ~original:code1 ~modified:code2 ~rename_set:vars =
+      let vars, env1, env2 = init_vars code1.env code2.env vars in
       Constr.mk_clause [] vars, env1, env2
     in
-    fold_comparators (init_vars :: hyps) ~original:(sub1, env1)
-      ~modified:(sub2, env2) ~rename_set:vars
+    fold_comparators (init_vars :: hyps)
+      ~original:{ code1 with env=env1 }
+      ~modified:{code2 with env=env2 }
+      ~rename_set:vars
   in
   info "\nHypotheses:\n%s\n%!" (Constr.to_string hyps);
-  let pre_mod, env2 = Pre.visit_sub env2 post sub2 in
-  let pre_combined, env1 = Pre.visit_sub env1 pre_mod sub1 in
+  let pre_mod, env2 = Pre.visit_sub env2 post code2.prog in
+  let pre_combined, env1 = Pre.visit_sub env1 pre_mod code1.prog in
   let goal = Constr.mk_clause [hyps] [pre_combined] in
   goal, env1, env2
 
 let compare_subs_empty : comparator * comparator =
-  let postcond ~original:(_, env1) ~modified:(_,env2) ~rename_set:_ =
+  let postcond ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set:_ =
     let ctx = Env.get_context env1 in
     let post = Constr.trivial ctx in
     post, env1, env2
@@ -146,24 +164,24 @@ let compare_subs_empty : comparator * comparator =
   postcond, hyps
 
 let compare_subs_empty_post : comparator * comparator =
-  let postcond ~original:(_, env1) ~modified:(_, env2) ~rename_set:_ =
+  let postcond ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set:_ =
     let ctx = Env.get_context env1 in
     let post = Constr.trivial ctx in
     post, env1, env2
   in
-  let hyps ~original:(_, env1) ~modified:(_, env2) ~rename_set =
+  let hyps ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set =
     let pre_eqs, env1, env2 = set_to_eqs env1 env2 rename_set in
     Constr.mk_clause [] pre_eqs, env1, env2
   in
   postcond, hyps
 
 let compare_subs_sp : comparator * comparator =
-  let postcond ~original:(_, env1) ~modified:(_, env2) ~rename_set:_ =
+  let postcond ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set:_ =
     let ctx = Env.get_context env1 in
     let post = Constr.trivial ctx in
     post, env1, env2
   in
-  let hyps ~original:(_, env1) ~modified:(_, env2) ~rename_set =
+  let hyps ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set =
     let sp_range = Pre.set_sp_range env1 in
     let pre_eqs, env1, env2 = set_to_eqs env1 env2 rename_set in
     Constr.mk_clause [] (sp_range :: pre_eqs), env1, env2
@@ -174,11 +192,11 @@ let compare_subs_smtlib
     ~smtlib_post:(smtlib_post : string)
     ~smtlib_hyp:(smtlib_hyp : string)
   : comparator * comparator =
-  let postcond ~original:(_, env1) ~modified:(_, env2) ~rename_set:_ =
-    Z3_utils.mk_smtlib2_compare env1 env2 smtlib_post, env1, env2
+  let postcond ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set:_ =
+    mk_smtlib2_compare env1 env2 smtlib_post, env1, env2
   in
-  let hyps ~original:(_, env1) ~modified:(_, env2) ~rename_set =
-    let smtlib = Z3_utils.mk_smtlib2_compare env1 env2 smtlib_hyp in
+  let hyps ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set =
+    let smtlib = mk_smtlib2_compare env1 env2 smtlib_hyp in
     let pre_eqs, env1, env2 = set_to_eqs env1 env2 rename_set in
     Constr.mk_clause [] (smtlib :: pre_eqs), env1, env2
   in
@@ -188,11 +206,11 @@ let compare_subs_eq
     ~pre_regs:(pre_regs : Var.Set.t)
     ~post_regs:(post_regs : Var.Set.t)
   : comparator * comparator =
-  let postcond ~original:(_, env1) ~modified:(_, env2) ~rename_set:_ =
+  let postcond ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set:_ =
     let post_eqs, env1, env2 = set_to_eqs env1 env2 post_regs in
     Constr.mk_clause [] post_eqs, env1, env2
   in
-  let hyps ~original:(_, env1) ~modified:(_, env2) ~rename_set:_ =
+  let hyps ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set:_ =
     let pre_eqs, env1, env2 = set_to_eqs env1 env2 pre_regs in
     Constr.mk_clause [] pre_eqs, env1, env2
   in
@@ -202,10 +220,10 @@ let compare_subs_constraints
     ~pre_conds:(pre_conds : Constr.t)
     ~post_conds:(post_conds : Constr.t)
   : comparator * comparator =
-  let postcond ~original:(_, env1) ~modified:(_, env2) ~rename_set:_ =
+  let postcond ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set:_ =
     Constr.mk_clause [] [post_conds], env1, env2
   in
-  let hyps ~original:(_, env1) ~modified:(_, env2) ~rename_set =
+  let hyps ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set =
     let pre_eqs, env1, env2 = set_to_eqs env1 env2 rename_set in
     Constr.mk_clause [] (pre_conds :: pre_eqs), env1, env2
   in
@@ -221,7 +239,7 @@ let compare_subs_fun : comparator * comparator =
     | None -> Bool.mk_false ctx
     | Some c_f -> Bool.mk_const_s ctx c_f
   in
-  let postcond ~original:(_, env1) ~modified:(_, env2) ~rename_set:_ =
+  let postcond ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set:_ =
     let ctx = Env.get_context env1 in
     (* Create the goal /\_f Called_f_original => Called_f_modified *)
     let no_additional_calls =
@@ -245,7 +263,7 @@ let compare_subs_fun : comparator * comparator =
     in
     Constr.mk_clause [] no_additional_calls, env1, env2
   in
-  let hyps ~original:(_, env1) ~modified:(_, env2) ~rename_set =
+  let hyps ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set =
     let ctx = Env.get_context env1 in
     (* Create the hypothesis /\_f !Called_f_modified *)
     let modified_not_called =
@@ -265,12 +283,12 @@ let compare_subs_fun : comparator * comparator =
   postcond, hyps
 
 let compare_subs_mem_eq : comparator * comparator =
-  let postcond ~original:(_, env1) ~modified:(_, env2) ~rename_set:_ =
+  let postcond ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set:_ =
     let ctx = Env.get_context env1 in
     let post = Constr.trivial ctx in
     post, env1, env2
   in
-  let hyps ~original:(_, env1) ~modified:(_, env2) ~rename_set =
+  let hyps ~original:{ env = env1; _ } ~modified:{ env = env2; _ } ~rename_set =
     let ctx = Env.get_context env1 in
     let mem1, env1 = Env.get_var env1 (Env.get_mem env1) in
     let mem2, env2 = Env.get_var env2 (Env.get_mem env2) in
@@ -281,5 +299,18 @@ let compare_subs_mem_eq : comparator * comparator =
     in
     let pre_eqs, env1, env2 = set_to_eqs env1 env2 rename_set in
     Constr.mk_clause [] (mem_eq :: pre_eqs), env1, env2
+  in
+  postcond, hyps
+
+let compare_subs_mem_init : comparator * comparator =
+  let postcond ~original ~modified ~rename_set:_ =
+    let ctx = Env.get_context original.env in
+    let post = Constr.trivial ctx in
+    post, original.env, modified.env
+  in
+  let hyps ~original ~modified ~rename_set:_ =
+    let pre, env1 = Pre.init_mem original.env original.mem in
+    let pre = Constr.mk_clause [] pre in
+    pre, env1, modified.env
   in
   postcond, hyps
--- wp/lib/bap_wp/tests/unit/test_compare.ml
+++ wp/lib/bap_wp/tests/unit/test_compare.ml
@@ -41,6 +41,9 @@ let assert_z3_compare (test_ctx : test_ctxt) (body1 : string) (body2 : string)
         print_z3_model solver exp real pre ~orig:env1 ~modif:env2)
     expected result
 
+let code_of_blk blk env = Comp.{env = env; prog = blk; mem = Memmap.empty}
+
+let code_of_sub sub env = Comp.{env = env; prog = sub; mem = Memmap.empty}
 
 let test_block_pair_1 (test_ctx : test_ctxt) : unit =
   let ctx = Env.mk_ctx () in
@@ -58,9 +61,10 @@ let test_block_pair_1 (test_ctx : test_ctxt) : unit =
   in
   let pre_regs = Var.Set.union (Var.Set.singleton x) (Var.Set.singleton y) in
   let post_regs = Var.Set.singleton z in
-  let compare_prop, env1, env2 = Comp.compare_blocks
+  let compare_prop, env1, env2 =
+    Comp.compare_blocks
       ~pre_regs ~post_regs
-      ~original:(blk1,env1) ~modified:(blk2,env2)
+      ~original:(code_of_blk blk1 env1) ~modified:(code_of_blk blk2 env2)
       ~smtlib_post:"" ~smtlib_hyp:"" in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Blk.to_string blk1) (Blk.to_string blk2)
@@ -84,7 +88,7 @@ let test_block_pair_2 (test_ctx : test_ctxt) : unit =
   let post_regs = Var.Set.singleton z in
   let compare_prop, env1, env2 = Comp.compare_blocks
       ~pre_regs ~post_regs
-      ~original:(blk1,env1) ~modified:(blk2,env2)
+      ~original:(code_of_blk blk1 env1) ~modified:(code_of_blk blk2 env2)
       ~smtlib_post:"" ~smtlib_hyp:"" in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Blk.to_string blk1) (Blk.to_string blk2)
@@ -127,7 +131,7 @@ let test_sub_pair_1 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -169,7 +173,7 @@ let test_sub_pair_2 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -205,7 +209,7 @@ let test_sub_pair_3 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -244,7 +248,7 @@ let test_sub_pair_4 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -283,7 +287,7 @@ let test_sub_pair_5 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -303,7 +307,7 @@ let test_sub_pair_6 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_empty_post in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -324,7 +328,7 @@ let test_sub_pair_7 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_empty_post in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -353,7 +357,7 @@ let test_sub_pair_fun_1 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_fun in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(main_sub1, env1) ~modified:(main_sub2, env2)
+      ~original:(code_of_sub main_sub1 env1) ~modified:(code_of_sub main_sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string main_sub1) (Sub.to_string main_sub2)
@@ -381,7 +385,7 @@ let test_sub_pair_fun_2 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_fun in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(main_sub1, env1) ~modified:(main_sub2, env2)
+      ~original:(code_of_sub main_sub1 env1) ~modified:(code_of_sub main_sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string main_sub1) (Sub.to_string main_sub2)
@@ -421,7 +425,7 @@ let test_sub_pair_fun_3 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_fun in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(main_sub1, env1) ~modified:(main_sub2, env2)
+      ~original:(code_of_sub main_sub1 env1) ~modified:(code_of_sub main_sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string main_sub1) (Sub.to_string main_sub2)
@@ -462,7 +466,7 @@ let test_sub_pair_fun_4 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_fun in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(main_sub1, env1) ~modified:(main_sub2, env2)
+      ~original:(code_of_sub main_sub1 env1) ~modified:(code_of_sub main_sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string main_sub1) (Sub.to_string main_sub2)
@@ -494,7 +498,7 @@ let test_fun_outputs_1 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(main_sub1, env1) ~modified:(main_sub2, env2)
+      ~original:(code_of_sub main_sub1 env1) ~modified:(code_of_sub main_sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string main_sub1)
@@ -514,7 +518,7 @@ let test_fun_outputs_2 (test_ctx : test_ctxt) : unit =
   let sub2 = Sub.with_name sub2 "test_call" in
   let main_sub1 =
     Bil.([ rdi := i64 1 ;
-           rsi := i64 2 ; 
+           rsi := i64 2 ;
            Bil_to_bir.call sub1 reg64_t]) |> bil_to_sub in
   let main_sub2 =
     Bil.([ rdi := i64 2 ;
@@ -529,7 +533,7 @@ let test_fun_outputs_2 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(main_sub1, env1) ~modified:(main_sub2, env2)
+      ~original:(code_of_sub main_sub1 env1) ~modified:(code_of_sub main_sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string main_sub1)
@@ -569,7 +573,7 @@ let test_fun_outputs_3 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(main_sub1, env1) ~modified:(main_sub2, env2)
+      ~original:(code_of_sub main_sub1 env1) ~modified:(code_of_sub main_sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string main_sub1)
@@ -609,7 +613,7 @@ let test_fun_outputs_4 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(main_sub1, env1) ~modified:(main_sub2, env2)
+      ~original:(code_of_sub main_sub1 env1) ~modified:(code_of_sub main_sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string main_sub1)
@@ -639,7 +643,7 @@ let test_sub_pair_mem_1 (test_ctx : test_ctxt) : unit =
   let post, hyps = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post] ~hyps:[hyps]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -680,7 +684,7 @@ let test_memory_model_1 (test_ctx : test_ctxt) : unit =
   let post2, hyps2 = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post1; post2] ~hyps:[hyps1; hyps2]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -721,7 +725,7 @@ let test_memory_model_2 (test_ctx : test_ctxt) : unit =
   let post2, hyps2 = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post1; post2] ~hyps:[hyps1; hyps2]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
@@ -770,7 +774,7 @@ let test_memory_model_3 (test_ctx : test_ctxt) : unit =
   let post2, hyps2 = Comp.compare_subs_eq ~pre_regs ~post_regs in
   let compare_prop, env1, env2 = Comp.compare_subs
       ~postconds:[post1; post2] ~hyps:[hyps1; hyps2]
-      ~original:(sub1, env1) ~modified:(sub2, env2)
+      ~original:(code_of_sub sub1 env1) ~modified:(code_of_sub sub2 env2)
   in
   assert_z3_compare test_ctx ~orig:env1 ~modif:env2
     (Sub.to_string sub1) (Sub.to_string sub2)
