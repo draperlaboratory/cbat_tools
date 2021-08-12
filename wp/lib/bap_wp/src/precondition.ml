dded063950788b0150662683527737ea5406e0bb
Cody Roux <croux@draper.com>
2021-08-12 16:38:43 -0400

    Fix flag bug

--- wp/lib/bap_wp/src/precondition.ml
+++ wp/lib/bap_wp/src/precondition.ml
@@ -1265,10 +1265,12 @@ let init_mem (env : Env.t) (mem : value memmap) : Constr.t list * Env.t =
   let mem_assoc (addr, word) =
     let addr = word_to_z3 z3_ctxt addr in
     let word = word_to_z3 z3_ctxt word in
+    (* z3_mem[addr] == word *)
     Bool.mk_eq z3_ctxt (Z3Array.mk_select z3_ctxt z3_mem addr) word
   in
   let z3_assoc = List.map ~f:mem_assoc bitv_pairs in
   let mk_cstr b = b |> Constr.mk_goal ".rodata_init" |> Constr.mk_constr in
+  info "Initializing values in %a\n%!" Var.pp mem_var;
   (List.map ~f:mk_cstr z3_assoc, env)
 
 
